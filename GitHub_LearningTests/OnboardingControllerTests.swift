6546c2d8ad0e5412a6eb556391d33ac489d7e48f
Amin <gajdalf4@seznam.cz>
2020-11-09 12:47:07 +0100

    Create initial setup for unit testing

---
+++ GitHub_LearningTests/OnboardingControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingControllerTests.swift
+//  GitHub_LearningTests
+//
+//  Created by Amin Ashhab on 09/11/2020.
+//  Copyright © 2020 Amin_Second_Test_Project. All rights reserved.
+//
+
+import XCTest
+@testable import GitHub_Learning
+
+class OnboardingControllerTests: XCTestCase {
+    
+    
+    
+}
