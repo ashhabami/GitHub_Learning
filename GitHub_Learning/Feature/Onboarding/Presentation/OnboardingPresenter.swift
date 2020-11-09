47ddbd7df43a7b77843411eaba1ea353a283087d
Amin <gajdalf4@seznam.cz>
2020-11-09 12:21:33 +0100

    Create OnboardingPresenter + OnboardingView

---
+++ GitHub_Learning/Feature/Onboarding/Presentation/OnboardingPresenter.swift
@@ -0,0 +1,27 @@
+//
+//  OnboardingPresenter.swift
+//  GitHub_Learning
+//
+//  Created by Amin Ashhab on 09/11/2020.
+//  Copyright © 2020 Amin_Second_Test_Project. All rights reserved.
+//
+
+import Foundation
+import CleanCore
+import CleanPlatform
+
+protocol OnboardingPresenter: Presenter {
+    
+}
+
+final class OnboardingPresenterImpl: BasePresenter<OnboardingView> {
+    
+    func viewDidLoad() {
+        
+    }
+    
+}
+
+extension OnboardingPresenterImpl: OnboardingPresenter {
+
+}
---
+++ GitHub_Learning/Feature/Onboarding/Presentation/OnboardingView.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingView.swift
+//  GitHub_Learning
+//
+//  Created by Amin Ashhab on 09/11/2020.
+//  Copyright © 2020 Amin_Second_Test_Project. All rights reserved.
+//
+
+import Foundation
+import CleanCore
+import CleanPlatform
+
+protocol OnboardingView: View {
+    
+}
