a787fce08469dc7a3033c8dd6f8eb296b1f53c9f
Amin <gajdalf4@seznam.cz>
2020-11-18 12:23:30 +0100

    Remove extra empty FakePresenter file

--- GitHub_LearningTests/Generic/Fakes/Presentation/FakePresenter.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FakeOnboardingPresenter.swift
-//  GitHub_LearningTests
-//
-//  Created by Amin Ashhab on 16/11/2020.
-//  Copyright © 2020 Amin_Second_Test_Project. All rights reserved.
-//
-
-import Foundation
