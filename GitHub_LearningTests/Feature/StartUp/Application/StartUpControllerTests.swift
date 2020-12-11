389c8691bc5f131cc811faa6396a1b3c6d74cf9f
Amin <gajdalf4@seznam.cz>
2020-12-11 14:22:38 +0100

    Fix

--- GitHub_LearningTests/Feature/StartUp/Application/StartUpControllerTests.swift
+++ GitHub_LearningTests/Feature/StartUp/Application/StartUpControllerTests.swift
@@ -12,80 +12,75 @@ import CleanCore
 
 class StartUpControllerTests: XCTestCase {
     private var sut: StartUpControllerImpl!
-    private var loadFinishedFacade: LoadOnboardingFinishedFacade!
-    private var keychainController: CredentialKeychainController!
-    private var loginLauncher: LoginLauncherController!
-    private var wf: Wireframe!
-    private var dashboardLauncher: DashboardLauncherController!
     
     private func setUpTests(
-        loadOnboardingFinishedFacade: LoadOnboardingFinishedFacade? = nil,
-        credentialKeychainController: CredentialKeychainController? = nil,
-        wireframe: Wireframe? = nil,
-        loginLauncherController: LoginLauncherController? = nil,
-        dashboardLauncherController: DashboardLauncherController? = nil
+        loadOnboardingFinishedFacade: LoadOnboardingFinishedFacade = LoadOnboardingFinishedFacadeDummy(),
+        loginLauncherController: LoginLauncherController = LoginLauncherControllerDummy(),
+        credentialKeychainController: CredentialKeychainController = CredentialKeychainControllerDummy(),
+        dashboardLauncherController: DashboardLauncherController = DashboardLauncherControllerDummy(),
+        wireframe: Wireframe = WireframeDummy()
     ) {
-        self.loadFinishedFacade = loadOnboardingFinishedFacade ?? LoadOnboardingFinishedFacadeDummy()
-        self.keychainController = credentialKeychainController ?? CredentialKeychainControllerDummy()
-        self.loginLauncher = loginLauncherController ?? LoginLauncherControllerDummy()
-        self.wf = wireframe ?? TestWireframe()
-        self.dashboardLauncher = dashboardLauncherController ?? DashboardLauncherControllerDummy()
         sut = StartUpControllerImpl(
-            loadOnboardingFinishedFacade: loadFinishedFacade,
-            loginLauncherController: loginLauncher,
-            credentialKeychainController: keychainController,
-            dashboardLauncherController: dashboardLauncher,
-            wireframe: wf)
+            loadOnboardingFinishedFacade: loadOnboardingFinishedFacade,
+            loginLauncherController: loginLauncherController,
+            credentialKeychainController: credentialKeychainController,
+            dashboardLauncherController: dashboardLauncherController,
+            wireframe: wireframe
+        )
     }
     
-    func test_givenLoadOnboardingFinishedFacadeIsCalledWithSuccessAndResponseIsTrue_whenStartUpIsCalled_thenLoadCredentialsIsCalled() {
-        // given
-        let facade = LoadOnboardingFinishedFacadeSuccessTrueStub()
+    func test_givenOnboardingIsFinished_whenStartUpIsCalled_thenLoadCredentialsIsCalled() {
+        // Given
+        let returnValue = onboardingFinishedResult(true)
+        let facade = LoadOnboardingFinishedFacadeDummy(returnValue: returnValue)
         let credentialKeychainController = CredentialKeychainControllerDummy()
         setUpTests(loadOnboardingFinishedFacade: facade, credentialKeychainController: credentialKeychainController)
         
-        // when
+        // When
         sut.startUp()
         
-        //Then
-        XCTAssertNotNil(credentialKeychainController.isLoaded)
-        XCTAssert(credentialKeychainController.isLoaded == true)
+        // Then
+        XCTAssertNotNil(credentialKeychainController.isCalled)
+        XCTAssert(credentialKeychainController.isCalled == true)
     }
     
-    func test_givenLoadOnboardingFinishedFacadeCallWithSuccessAndResponseIsFalse_whenStartUpIsCalled_thenSetOnboardingAsRootIsCalled() {
-        // given
-        let facade = LoadOnboardingFinishedFacadeSuccessFalseStub()
-        let credentialKeychainController = CredentialKeychainControllerDummy()
-        let wireframe = TestWireframe()
-        setUpTests(loadOnboardingFinishedFacade: facade, credentialKeychainController: credentialKeychainController,wireframe: wireframe)
+    func test_givenOnboardingIsNotFinished_whenStartUpIsCalled_thenSetOnboardingAsRootIsCalled() {
+        // Given
+        let returnValue = onboardingFinishedResult(false)
+        let facade = LoadOnboardingFinishedFacadeDummy(returnValue: returnValue)
+        let wireframe = WireframeDummy()
+        setUpTests(loadOnboardingFinishedFacade: facade, wireframe: wireframe)
         
-        // when
+        // When
         sut.startUp()
         
-        //Then
+        // Then
         XCTAssertNotNil(wireframe.isOnboardingSetAsRoot)
         XCTAssert(wireframe.isOnboardingSetAsRoot == true)
     }
     
-    func test_givenLoadOnboardingFinishedFacadeCallWithFailure_whenStartUpIsCalled_thenSetOnboardingAsRootIsCalled() {
-        // given
-        let facade = LoadOnboardingFinishedFacadeFailureStub()
-        let credentialKeychainController = CredentialKeychainControllerDummy()
-        let wireframe = TestWireframe()
-        setUpTests(loadOnboardingFinishedFacade: facade, credentialKeychainController: credentialKeychainController,wireframe: wireframe)
+    func test_givenLoadOnboardingFails_whenStartUpIsCalled_thenSetOnboardingAsRootIsCalled() {
+        // Given
+        let error = LocalStorageError.loadFailed("Load Failed")
+        let returnValue = onboardingFailureResult(error)
+        let facade = LoadOnboardingFinishedFacadeDummy(returnValue: returnValue)
+        let wireframe = WireframeDummy()
+        setUpTests(loadOnboardingFinishedFacade: facade, wireframe: wireframe)
         
-        // when
+        // When
         sut.startUp()
         
-        //Then
+        // Then
         XCTAssertNotNil(wireframe.isOnboardingSetAsRoot)
         XCTAssert(wireframe.isOnboardingSetAsRoot == true)
     }
     
     func test_givenLoadCredentailsIsCalledWithSuccess_whenStartUpIsCalled_thenDashboardLauncherCallsLaunchDashboardWith() {
-        // given
-        let facade = LoadOnboardingFinishedFacadeSuccessTrueStub()
-        let credentialKeychainController = CredentialKeychainControllerSuccessStub()
+        // Given
+        let facade = LoadOnboardingFinishedFacadeDummy(returnValue: onboardingFinishedResult(true))
+        let email = "success@gmail.com"
+        let returnValue = credentailsLoadedSuccessResult(email)
+        let credentialKeychainController = CredentialKeychainControllerDummy(returnValue: returnValue)
         let dashboardLauncherController = DashboardLauncherControllerDummy()
         setUpTests(
             loadOnboardingFinishedFacade: facade,
@@ -93,31 +88,33 @@ class StartUpControllerTests: XCTestCase {
             dashboardLauncherController: dashboardLauncherController
         )
         
-        // when
+        // When
         sut.startUp()
         
-        //Then
+        // Then
         XCTAssertNotNil(dashboardLauncherController.launched)
         XCTAssert(dashboardLauncherController.launched == true)
-        XCTAssert(dashboardLauncherController.email == credentialKeychainController.loadedCredentials)
+        XCTAssert(dashboardLauncherController.email == email)
         XCTAssert(dashboardLauncherController.point == DashboardLaunchPoint.startUp)
     }
     
     func test_givenLoadCredentailsIsCalledWithFailure_whenStartUpIsCalled_thenLoginLauncherCallsLaunchLoginAfter() {
-        // given
-        let facade = LoadOnboardingFinishedFacadeSuccessTrueStub()
-        let credentialKeychainController = CredentialKeychainControllerFailureStub()
+        // Given
+        let facade = LoadOnboardingFinishedFacadeDummy(returnValue: onboardingFinishedResult(true))
         let loginLauncherController = LoginLauncherControllerDummy()
+        let returnValue = credentailsLoadedFailureResult(LocalStorageError.loadFailed("Load Failed"))
+        let credentialKeychainController = CredentialKeychainControllerDummy(returnValue: returnValue)
         setUpTests(
             loadOnboardingFinishedFacade: facade,
-            credentialKeychainController: credentialKeychainController,
-            loginLauncherController: loginLauncherController
+            loginLauncherController: loginLauncherController,
+            credentialKeychainController: credentialKeychainController
         )
         
-        // when
+        // When
         sut.startUp()
         
-        //Then
+        // Then
+        XCTAssert(credentialKeychainController.isCalled == true)
         XCTAssert(credentialKeychainController.isLoaded == false)
         XCTAssertNotNil(loginLauncherController.launched)
         XCTAssert(loginLauncherController.launched == true)
@@ -125,64 +122,62 @@ class StartUpControllerTests: XCTestCase {
     }
     
     private class LoadOnboardingFinishedFacadeDummy: LoadOnboardingFinishedFacade {
-        func load(_ request: LoadOnboardingFinishedRequest, completion: @escaping ((Result<LoadOnboardingFinishedResponse>) -> Void)) {}
-    }
-    
-    private class LoadOnboardingFinishedFacadeSuccessTrueStub: LoadOnboardingFinishedFacade {
+        private let returnValue: Result<LoadOnboardingFinishedResponse>?
+
+        init(returnValue: Result<LoadOnboardingFinishedResponse>? = nil) {
+            self.returnValue = returnValue
+        }
+        
         func load(_ request: LoadOnboardingFinishedRequest, completion: @escaping ((Result<LoadOnboardingFinishedResponse>) -> Void)) {
-            completion(.success(response: LoadOnboardingFinishedResponse(isFinished: true)))
+            if let returnValue = returnValue {
+                completion(returnValue)
+            }
         }
     }
     
-    private class LoadOnboardingFinishedFacadeSuccessFalseStub: LoadOnboardingFinishedFacade {
-        func load(_ request: LoadOnboardingFinishedRequest, completion: @escaping ((Result<LoadOnboardingFinishedResponse>) -> Void)) {
-            completion(.success(response: LoadOnboardingFinishedResponse(isFinished: false)))
-        }
+    private func onboardingFinishedResult(_ isFinished: Bool) -> Result<LoadOnboardingFinishedResponse> {
+        .success(response: LoadOnboardingFinishedResponse(isFinished: isFinished))
     }
     
-    private class LoadOnboardingFinishedFacadeFailureStub: LoadOnboardingFinishedFacade {
-        func load(_ request: LoadOnboardingFinishedRequest, completion: @escaping ((Result<LoadOnboardingFinishedResponse>) -> Void)) {
-            completion(.failure(error: LocalStorageError.loadFailed("Load Failed")))
-        }
+    private func onboardingFailureResult(_ error: Error) -> Result<LoadOnboardingFinishedResponse> {
+        .failure(error: error)
     }
     
     private class CredentialKeychainControllerDummy: TestController, CredentialKeychainController {
+        var isCalled: Bool?
         var isLoaded: Bool?
+        private let returnValue: Result<LoadCredentialsKeychainResponse>?
+        
+        init(returnValue: Result<LoadCredentialsKeychainResponse>? = nil) {
+            self.returnValue = returnValue
+        }
     
         func storeCredentials(_ credintals: String) {}
         func deleteCredentials() {}
         
         func loadCredentials(_ completion: @escaping (Result<LoadCredentialsKeychainResponse>) -> Void) {
-            isLoaded = true
+            if let returnValue = returnValue {
+                switch returnValue {
+                case .success:
+                    isCalled = true
+                    isLoaded = true
+                case .failure:
+                    isCalled = true
+                    isLoaded = false
+                }
+                completion(returnValue)
+            } else {
+                isCalled = true
+            }
         }
     }
     
-    private class CredentialKeychainControllerSuccessStub: TestController, CredentialKeychainController {
-        var isLoaded: Bool?
-        var loadedCredentials: String?
-    
-        func storeCredentials(_ credintals: String) {}
-        func deleteCredentials() {}
-        
-        func loadCredentials(_ completion: @escaping (Result<LoadCredentialsKeychainResponse>) -> Void) {
-            isLoaded = true
-            let credentials = "Success"
-            self.loadedCredentials = credentials
-            completion(.success(response: LoadCredentialsKeychainResponse(credentials: credentials)))
-        }
+    private func credentailsLoadedSuccessResult(_ credentails: String) -> Result<LoadCredentialsKeychainResponse> {
+        .success(response: LoadCredentialsKeychainResponse(credentials: credentails) )
     }
     
-    private class CredentialKeychainControllerFailureStub: TestController, CredentialKeychainController {
-        var isLoaded: Bool?
-        var loadedCredentials: String?
-    
-        func storeCredentials(_ credintals: String) {}
-        func deleteCredentials() {}
-        
-        func loadCredentials(_ completion: @escaping (Result<LoadCredentialsKeychainResponse>) -> Void) {
-            isLoaded = false
-            completion(.failure(error: LocalStorageError.loadFailed("Load Failed")))
-        }
+    private func credentailsLoadedFailureResult(_ error: Error) -> Result<LoadCredentialsKeychainResponse> {
+        .failure(error: error)
     }
     
     
@@ -208,7 +203,7 @@ class StartUpControllerTests: XCTestCase {
         }
     }
     
-    private class TestWireframe: Wireframe {
+    private class WireframeDummy: Wireframe {
         var isOnboardingSetAsRoot: Bool?
         
         func launchLoginAfter(_ point: LoginLaunchPoint) {}
