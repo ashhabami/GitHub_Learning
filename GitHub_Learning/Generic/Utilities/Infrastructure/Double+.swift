c85d5e1285132896b6e8c2f86583c14d68fb8965
Amin <gajdalf4@seznam.cz>
2020-12-24 13:18:01 +0100

    Add Double to String convertion logic extension

---
+++ GitHub_Learning/Generic/Utilities/Infrastructure/Double+.swift
@@ -0,0 +1,15 @@
+//
+//  Double+.swift
+//  GitHub_Learning
+//
+//  Created by Amin Ashhab on 20.12.2020.
+//  Copyright © 2020 Amin_Second_Test_Project. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    func toString() -> String {
+        return String(format: "%.1f",self)
+    }
+}
