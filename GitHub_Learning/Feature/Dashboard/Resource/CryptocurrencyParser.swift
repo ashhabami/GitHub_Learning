cdd930b07669f28b270946e6a535b509a4b3512c
Amin <gajdalf4@seznam.cz>
2020-12-24 13:16:39 +0100

    Add CryptocurrencyPrice Resource + Parser

---
+++ GitHub_Learning/Feature/Dashboard/Resource/CryptocurrencyParser.swift
@@ -0,0 +1,27 @@
+//
+//  CryptocurrencyPriceParser.swift
+//  GitHub_Learning
+//
+//  Created by Amin Ashhab on 20.12.2020.
+//  Copyright © 2020 Amin_Second_Test_Project. All rights reserved.
+//
+
+import Foundation
+import CleanCore
+
+protocol CryptocurrencyParser {
+    func parse(_ responseBody: DeserializedBody) throws -> Cryptocurrency
+}
+
+class CryptocurrencyParserImpl: Parser, CryptocurrencyParser {
+    func parse(_ responseBody: DeserializedBody) throws -> Cryptocurrency {
+        self.deserializedBody = responseBody
+        let array = deserializedBody as? [Any]
+        let dictionary = array?[0] as? [String:Any]
+        let price = dictionary?["current_price"] as? Int
+        let symbol = dictionary?["symbol"] as? String
+        let image = dictionary?["image"] as? String
+        let priceChange = dictionary?["price_change_percentage_24h"] as? Double
+        return Cryptocurrency(imageUrl: image, symbol: symbol, priceChange: priceChange, price: price)
+    }
+}
