49901ce65d25911b5f16a1035c5dbf0234906a5e
Amin <gajdalf4@seznam.cz>
2020-12-24 13:16:00 +0100

    Add cryptocurrencyPrice presenting logic into DashboardPresenter + Dashboard CryptocurrencyViewModel + extend DashbordView with additional logic related to CryptocurrencyPrice

---
+++ GitHub_Learning/Feature/Dashboard/Presentation/CryptocurrencyViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  DashboardViewModel.swift
+//  GitHub_Learning
+//
+//  Created by Amin Ashhab on 22.12.2020.
+//  Copyright © 2020 Amin_Second_Test_Project. All rights reserved.
+//
+
+import Foundation
+
+enum PriceChangeDirection {
+    case positive
+    case negative
+    case neutral
+}
+
+struct CryptocurrencyViewModel {
+    let imageUrl: URL?
+    let price: String
+    let priceChangePercentage: String
+    let priceChange: PriceChangeDirection
+    let symbol: String
+}
--- GitHub_Learning/Feature/Dashboard/Presentation/DashboardPresenter.swift
+++ GitHub_Learning/Feature/Dashboard/Presentation/DashboardPresenter.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CleanCore
 
-protocol DashboardPresenter: Presenter {
+protocol DashboardPresenter: Presenter, Listener {
     func logOut()
 }
 
@@ -27,6 +27,43 @@ final class DashboardPresenterImpl: BasePresenter<DashboardView> {
     
     func viewDidLoad() {
         view?.setEmail(dashboardCotroller.getEmail())
+        dashboardCotroller.subscribe(self, errorBlock: nil, updateBlock: { _ in self.updateBlock()})
+    }
+    
+    private func makeViewModelFrom(_ cryprocurrency: Cryptocurrency?) -> CryptocurrencyViewModel? {
+        guard let cryprocurrencyUnwraped = cryprocurrency else { return nil }
+        let url = cryprocurrencyUnwraped.imageUrl ?? ""
+        let price = String(cryprocurrencyUnwraped.price ?? 0)
+        var priceChangePercentage = cryprocurrencyUnwraped.priceChange?.toString() ?? "N&N"
+        let symbol = cryprocurrencyUnwraped.symbol?.uppercased() ?? "N&N"
+        let priceChange: PriceChangeDirection
+        
+        priceChangePercentage += " %"
+        if priceChangePercentage.contains("-") {
+            priceChange = .negative
+        } else if priceChangePercentage.contains("N&N") || priceChangePercentage.contains("0.0") {
+            priceChange = .neutral
+            priceChangePercentage.insert(contentsOf: "+ ", at: priceChangePercentage.startIndex)
+        } else {
+            priceChange = .positive
+            priceChangePercentage.insert(contentsOf: "+ ", at: priceChangePercentage.startIndex)
+        }
+        
+        return CryptocurrencyViewModel(
+            imageUrl: URL(string: url),
+            price: price,
+            priceChangePercentage: priceChangePercentage,
+            priceChange: priceChange,
+            symbol: symbol
+        )
+    }
+    
+    private func updateBlock() {
+        guard let viewModel = makeViewModelFrom(dashboardCotroller.cryptocurrency) else { return }
+        view?.setCryptocurrencyPrice(viewModel.price)
+        view?.setCryptocurrencyImage(viewModel.imageUrl)
+        view?.setCryptocurrencyPriceChange(viewModel.priceChangePercentage, direction: viewModel.priceChange)
+        view?.setCryptocurrencySymbol("\(viewModel.symbol)/USD")
     }
 }
 
